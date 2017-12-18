534dfda8e8616fb4fc6ab00b92aaaaf4b223de58
Taysir Al-Shareef Pinero <taysirasp@gmail.com>
2017-12-18 22:47:49 +0100

    Instanciado el MainActivityEvents, creados los LinearLayouts con sus correspondientes fragments mediante el empleo de FragmentTransaction, implementados los métodos de las interfaces de los frtagments en el mainActivityEvents e instanciado el escuchador MainActivityEvents para los listener de los fragments

--- examenAndroid/src/main/java/com/example/examenandroid/MainActivity.java
+++ examenAndroid/src/main/java/com/example/examenandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.examenandroid;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,7 +12,7 @@ Para usar nuestra librería vamos a importarla en el build.gradle del módulo "e
  */
 
 public class MainActivity extends AppCompatActivity {
-    // Declaración de las variables de los fragments que se encuentran en el mainActivity
+    // Declaración de las variables de los fragments que se encuentran en el mainActivity gracias a que hemos importado la librería
     private LoginFragment loginFragment;
     private RegisterFragment registerFragment;
     /*
@@ -24,6 +25,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        Como el MainActivity será lo primero que vamos ejecutar al arrancar la aplicación y el onCreate es el método
+        que se ejecutará en primera instancia, vamos usarlo para instanciar los fragments y el events.
+         */
+         /*
+          Como el constructor debe de recibir el mainActivity, le pasamos mediante "this" el propio MainActivity dado que
+          estamos a la propia clase MainActivity
+         */
+        this.setMainActivityEvents(new MainActivityEvents(this));
+        /*
+        En un principio  asignabamos directamente a cada variable de un fragment su correspondiente XML con el método
+        propio de las views llamado "findViewByid()" y haciendo uso del fichero id al que se accede por R..id.idAsignada.
+        Para mejorar la optimización y agregar de forma dinámica fragments a nuestra app, vamos a crear un linearLayout
+        que contendrá a su vez linearLayouts hijas las cuáles cada una tendrá un fragment.
+        De esta forma, podremos gestionar estos fragmentos (añadirlos, borrarlos,etc). Este linearLayout se ecuentra creado
+        desde el XML del mainActivity.
+        Por último, para poder añadir dichos fragments al layout utilizaremos una transacción para operar estas acciones.
+        Las transacciones contienen el método  getSupportFragmentManager().beginTransaction() que permite administrar
+        fragmentos e iniciar una transacción.
+
+         */
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //Con la operación add especificamos el contenedor donde queremos meter el fragment, el fragment que queremos meter y un identificador para ese fragment
+        transaction.add(R.id.loginLayout, this.getLoginFragment(), "logFragment");
+        transaction.add(R.id.registerLayout, this.getRegisterFragment(), "regFragment");
+        /*
+        Ahora establecemos el esuchador de los eventos que ocurran en los fragments.
+        Cada fragment tiene su propio events, pero todos ellos llamarán mediante sus correspondientes listener (variables de las interfaces)
+        a quién implemente los métodos de dichas interfaces. El MainActivityEvents va a implementar los métodos de las interfaces
+        de los fragments.
+        Por lo tanto, los fragmentos tanto login como register tienen que saber que su escuchador es el MainActivityEvents que es el que
+        implementa sus métodos para que desde los events de los fragments se pueda llamar a los que implementen esos métodos(MainActivityEvents),
+        pues si no, estaría a null.
+         */
+        this.loginFragment.setLoginFragmentListener(this.getMainActivityEvents());
+        this.registerFragment.setRegisterFragmentListener(this.getMainActivityEvents());
+        //Como inicialmente queremos que se muestre el loginFragment y oculte el registerFragment usamos show y hide respectivamente.
+        //Estos métodos se pueden usar gracias a que como hemos dicho antes, el transaction permite gestionar los frtagments.
+        transaction.hide(this.getRegisterFragment());
+        transaction.show(this.getLoginFragment());
+        /*
+        Por último, para que se lleven a cabo los cambios debemos de "firmar" que es lo que se conoce como comitear para que se
+        produzcan los cambios
+         */
+        transaction.commit(); // comiteamos
+
     }
 
     public LoginFragment getLoginFragment() {
--- examenAndroid/src/main/java/com/example/examenandroid/MainActivityEvents.java
+++ examenAndroid/src/main/java/com/example/examenandroid/MainActivityEvents.java
@@ -1,8 +1,47 @@
 package com.example.examenandroid;
 
+import com.example.mylib.fragment.LoginFragmentListener;
+import com.example.mylib.fragment.RegisterFragmentListener;
+
 /**
  * Created by tay on 18/12/17.
  */
 
-public class MainActivityEvents {
+/*
+Vamos a hacer que implemente los métodos de los dragment mediante las interfaces loginFragmentlistener y registerFragmentListener
+ */
+public class MainActivityEvents implements LoginFragmentListener, RegisterFragmentListener { //
+    /*
+    Declaramos y recibimos por parámetro la variable de tipo MainActivity pues ciertas acciones,
+    requerirán de la llamada de esta activity
+     */
+
+    private MainActivity mainActivity;
+
+    public MainActivityEvents(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+    public MainActivity getMainActivity() {
+        return mainActivity;
+    }
+
+    public void setMainActivity(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+    @Override
+    public void OnRegisteredClicked() {
+
+    }
+
+    @Override
+    public void OnLoginClicked() {
+
+    }
+
+    @Override
+    public void OnClickRegistered() {
+
+    }
 }
