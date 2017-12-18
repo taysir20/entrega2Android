f14e47916ada3d7b5f43db1d8bbc2447c8de3856
Taysir Al-Shareef Pinero <taysirasp@gmail.com>
2017-12-18 20:59:49 +0100

    Importación de la librería en el gradle del módulo examenAndroid, creadas las variables de loginFragment y RegisterFragment gracias a que la librería ha sido importada, y el creado el mainActivityEvents vacío

--- examenAndroid/src/main/java/com/example/examenandroid/MainActivity.java
+++ examenAndroid/src/main/java/com/example/examenandroid/MainActivity.java
@@ -3,11 +3,50 @@ package com.example.examenandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.mylib.fragment.LoginFragment;
+import com.example.mylib.fragment.RegisterFragment;
+
+/*
+Para usar nuestra librería vamos a importarla en el build.gradle del módulo "examen android"
+ */
+
 public class MainActivity extends AppCompatActivity {
+    // Declaración de las variables de los fragments que se encuentran en el mainActivity
+    private LoginFragment loginFragment;
+    private RegisterFragment registerFragment;
+    /*
+    Declaración del MainActivityEvents que gestionará todos los eventos que se llevan a cabo en todos los componentes
+    del MainActivity.
+     */
+    private MainActivityEvents mainActivityEvents;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public LoginFragment getLoginFragment() {
+        return loginFragment;
+    }
+
+    public void setLoginFragment(LoginFragment loginFragment) {
+        this.loginFragment = loginFragment;
+    }
+
+    public RegisterFragment getRegisterFragment() {
+        return registerFragment;
+    }
+
+    public void setRegisterFragment(RegisterFragment registerFragment) {
+        this.registerFragment = registerFragment;
+    }
+
+    public MainActivityEvents getMainActivityEvents() {
+        return mainActivityEvents;
+    }
+
+    public void setMainActivityEvents(MainActivityEvents mainActivityEvents) {
+        this.mainActivityEvents = mainActivityEvents;
     }
 }
---
+++ examenAndroid/src/main/java/com/example/examenandroid/MainActivityEvents.java
@@ -0,0 +1,8 @@
+package com.example.examenandroid;
+
+/**
+ * Created by tay on 18/12/17.
+ */
+
+public class MainActivityEvents {
+}
