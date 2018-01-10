f80ed35feb995a17fa59f9505183bbc36b7d35cf
Taysir Al-Shareef Pinero <taysirasp@gmail.com>
2018-01-10 16:31:52 +0100

    Añadidos estilos Primera parte Actividad

---
+++ dintentregas/src/main/java/com/example/dintentregas/MainActivityFragment2.java
@@ -0,0 +1,22 @@
+package com.example.dintentregas;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment2 extends Fragment {
+
+    public MainActivityFragment2() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main2, container, false);
+    }
+}
