2aefdc24fc5c9ee7bdd3d50be20d81c9f3804fb3
Taysir Al-Shareef Pinero <taysirasp@gmail.com>
2018-01-11 16:28:18 +0100

    Añadido MApFragment al MainActivity de forma estática

--- pmdmentregas/src/main/java/com/example/pmdmentregas/MainActivity.java
+++ pmdmentregas/src/main/java/com/example/pmdmentregas/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 
 
 import com.example.pmdmentregas.firebase.FirebaseAdmin;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
     private MainActivityEvents mainActivityEvents;
     private FirebaseAdmin firebaseAdmin;
+    private SupportMapFragment mapFragment; //creamos el SupportMapFragment que es más compatible que un MapFragment
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         this.setFirebaseAdmin(new FirebaseAdmin());
         this.setMainActivityEvents(new MainActivityEvents(this));
+       /*
+       Mediante el método getSpportFragemntManagr que ya conocemos pues es un método exclusivo de clases
+       que extienden AppCompatActivity vamos a asignar el componente MapFragment a la variable SupportMapFragment
+        */
+        this.mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapFragment);
 
     }
 
@@ -33,5 +41,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void setFirebaseAdmin(FirebaseAdmin firebaseAdmin) {
         this.firebaseAdmin = firebaseAdmin;
+    }
+
+    public SupportMapFragment getMapFragment() {
+        return mapFragment;
+    }
+
+    public void setMapFragment(SupportMapFragment mapFragment) {
+        this.mapFragment = mapFragment;
     }
 }
